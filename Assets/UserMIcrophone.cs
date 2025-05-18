af3ae4d44bcb0b8b9f39de19666a09f9ecffb4ba
bcaliva21 <bcaliva21@gmail.com>
2025-05-17 20:00:34 -0700

    feat: prolong particle life; adjust max speed;

--- Assets/UserMIcrophone.cs
+++ Assets/UserMIcrophone.cs
@@ -19,7 +19,7 @@ public class UserMicrophone : MonoBehaviour
     public float[] spectrumData;
     public AudioPitchEstimator audioPitchEstimator;
     float emitTimer = 0f;
-    float emitInterval = 0.05f; // Emit every 0.1 seconds
+    float emitInterval = 0.02f; // Emit every 0.1 seconds
 
     void Start()
     {
@@ -72,71 +72,146 @@ public class UserMicrophone : MonoBehaviour
                     var psShape = particleSystem.shape;
                     var psMain = particleSystem.main;
 
+
                     Debug.Log("Rounded Pitch: " + roundedPitch);
-                    if (roundedPitch >= 186 && roundedPitch <= 206) {
+                    // I know... should abstract this, but I wanted to test it
+                    // and having chatGPT generate this slop is relatively quick
+                    if (roundedPitch >= 300 && roundedPitch < 330)
+                    {
                         int index = 0;
                         psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
                         Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
                         psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(1f, 0f, 0f, 1f); // Red (G3)
+                        psMain.startColor = new Color(1f, 0f, 0f, 1f); // Red (C#4)
                         particleSystem.Emit(1);
                     }
-                    else if (roundedPitch >= 210 && roundedPitch <= 230) {
+                    else if (roundedPitch >= 330 && roundedPitch < 350)
+                    {
+                        int index = 1;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(1f, 0.5f, 0f, 1f); // Orange (D4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 350 && roundedPitch < 380)
+                    {
+                        int index = 2;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(1f, 1f, 0f, 1f); // Yellow (D#4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 380 && roundedPitch < 410)
+                    {
+                        int index = 3;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0f, 1f, 0f, 1f); // Green (E4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 410 && roundedPitch < 440)
+                    {
+                        int index = 4;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0f, 0f, 1f, 1f); // Blue (F4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 440 && roundedPitch < 470)
+                    {
+                        int index = 5;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.29f, 0f, 0.51f, 1f); // Indigo (F#4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 470 && roundedPitch < 500)
+                    {
+                        int index = 6;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.56f, 0f, 1f, 1f); // Violet (G4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 500 && roundedPitch < 530)
+                    {
+                        int index = 7;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(1f, 1f, 1f, 1f); // White (G#4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 530 && roundedPitch < 560)
+                    {
+                        int index = 8;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.5f, 0.25f, 0f, 1f); // Brown (A4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 560 && roundedPitch < 590)
+                    {
+                        int index = 9;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(1f, 0f, 1f, 1f); // Magenta (A#4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 590 && roundedPitch < 620)
+                    {
+                        int index = 10;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0f, 1f, 1f, 1f); // Cyan (B4)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 620 && roundedPitch < 650)
+                    {
+                        int index = 11;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.5f, 0.5f, 0f, 1f); // Olive (C5)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 650 && roundedPitch < 680)
+                    {
+                        int index = 12;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.8f, 0.2f, 0.8f, 1f); // Orchid (C#5)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 680 && roundedPitch < 710)
+                    {
+                        int index = 13;
+                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
+                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
+                        psTransform.rotation = Quaternion.LookRotation(direction);
+                        psMain.startColor = new Color(0.3f, 0.7f, 0.3f, 1f); // Mint Green (D5)
+                        particleSystem.Emit(1);
+                    }
+                    else if (roundedPitch >= 710 && roundedPitch < 740)
+                    {
                         int index = 14;
                         psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
                         Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
                         psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(1f, 0.5f, 0f, 1f); // Orange (A3)
+                        psMain.startColor = new Color(0.6f, 0f, 0f, 1f); // Dark Red (D#5)
                         particleSystem.Emit(1);
                     }
-                    else if (roundedPitch >= 237 && roundedPitch <= 257) {
-                        int index = 28;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(1f, 1f, 0f, 1f); // Yellow (B3)
-                        particleSystem.Emit(1);
-                    }
-                    else if (roundedPitch >= 251 && roundedPitch <= 271) {
-                        int index = 42;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(0f, 1f, 0f, 1f); // Green (C4)
-                        particleSystem.Emit(1);
-                    }
-                    else if (roundedPitch >= 284 && roundedPitch <= 304) {
-                        int index = 56;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(0f, 0f, 1f, 1f); // Blue (D4)
-                        particleSystem.Emit(1);
-                    }
-                    else if (roundedPitch >= 320 && roundedPitch <= 340) {
-                        int index = 71;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(0.29f, 0f, 0.51f, 1f); // Indigo (E4)
-                        particleSystem.Emit(1);
-                    }
-                    else if (roundedPitch >= 360 && roundedPitch <= 380) {
-                        int index = 85;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(0.56f, 0f, 1f, 1f); // Violet (F#4)
-                        particleSystem.Emit(1);
-                    }
-                    else if (roundedPitch >= 382 && roundedPitch <= 402) {
-                        int index = 99;
-                        psTransform.position = sphereSurfacePoints.surfacePoints[index].position;
-                        Vector3 direction = sphereSurfacePoints.surfacePoints[index].normal;
-                        psTransform.rotation = Quaternion.LookRotation(direction);
-                        psMain.startColor = new Color(1f, 1f, 1f, 1f); // White (G4)
-                        particleSystem.Emit(1);
-                    }
+
                 }
             }
             else
