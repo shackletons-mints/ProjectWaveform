99c3ec49c8125197056458f5adcf024ad349a91e
Bradley Caliva <bcaliva21@gmail.com>
2025-06-24 16:05:01 -0700

    feat: add paint assets

--- Assets/Scripts/Runtime/ParticlePlaneCollisionManager.cs
+++ Assets/Scripts/Runtime/ParticlePlaneCollisionManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.XR.ARFoundation;
 
 public class ARPlaneColliderEnabler : MonoBehaviour
 {
+	[SerializeField] private Material planeMaterial;
     public ARPlaneManager planeManager;
 
     void OnEnable()
@@ -20,6 +21,9 @@ public class ARPlaneColliderEnabler : MonoBehaviour
         foreach (var plane in args.added)
         {
             AddOrUpdateCollider(plane);
+			AddPaintable(plane);
+			SetupMeshRenderer(plane);
+			SetupRigidbody(plane);
         }
 
         foreach (var plane in args.updated)
@@ -31,9 +35,52 @@ public class ARPlaneColliderEnabler : MonoBehaviour
     void AddOrUpdateCollider(ARPlane plane)
     {
         if (!plane.TryGetComponent<MeshCollider>(out var collider))
+		{
             collider = plane.gameObject.AddComponent<MeshCollider>();
+		}
 
         collider.sharedMesh = plane.GetComponent<MeshFilter>().mesh;
     }
+
+	void AddPaintable(ARPlane plane)
+	{
+        if (!plane.TryGetComponent<Paintable>(out var script))
+        {
+            plane.gameObject.AddComponent<Paintable>();
+        }
+	}
+
+	void SetupMeshRenderer(ARPlane plane)
+	{
+		if (!plane.TryGetComponent<MeshRenderer>(out var meshRenderer))
+		{
+			meshRenderer = plane.gameObject.AddComponent<MeshRenderer>();
+		}
+
+		meshRenderer.material = planeMaterial;
+
+		meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+		meshRenderer.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.BlendProbes;
+		meshRenderer.reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.BlendProbes;
+		meshRenderer.probeAnchor = null;
+		meshRenderer.allowOcclusionWhenDynamic = true;
+		meshRenderer.renderingLayerMask = 1 << 1;
+
+	}	
+	void SetupRigidbody(ARPlane plane)
+	{
+		if (!plane.TryGetComponent<Rigidbody>(out var rb))
+		{
+			rb = plane.gameObject.AddComponent<Rigidbody>();
+		}
+
+		rb.mass = 1f;
+		rb.linearDamping = 0f;
+		rb.angularDamping = 0.05f;
+		rb.useGravity = false;
+		rb.isKinematic = true;
+		rb.interpolation = RigidbodyInterpolation.None;
+		rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+	}
 }
 
