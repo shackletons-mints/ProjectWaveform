c7ae9542e0983a79933ac3fa2ca5aceee5168eeb
Bradley Caliva <bcaliva21@gmail.com>
2025-12-30 14:41:52 -0800

    fix: audio playback issue

--- Assets/Scripts/Runtime/AudioVisualization/AudioVisualizer.cs
+++ Assets/Scripts/Runtime/AudioVisualization/AudioVisualizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 using Utilities;
 
@@ -39,6 +40,8 @@ namespace AudioVisualization
         private Vector3 previousSpherePosition;
         public SphereSurfacePoints sphereSurfacePoints;
         public Material rippleShader;
+        public AudioMixerGroup silentOutput;
+        public AudioMixerGroup normalOutput;
 
         public int previousPitchClass;
         public int spectrumSize = 1024;
@@ -105,6 +108,7 @@ namespace AudioVisualization
 
                     while (!(Microphone.GetPosition(null) > 0)) { }
 
+                    audioSource.outputAudioMixerGroup = silentOutput;
                     audioSource.Play();
                 }
                 else
@@ -115,6 +119,7 @@ namespace AudioVisualization
             }
             else
             {
+                audioSource.outputAudioMixerGroup = normalOutput;
                 audioClip = _audioClip;
                 audioSource = _audioSource;
                 audioSource.Play();
